09d2e6e26320e9c83f63848d40b5e982ce64f067
Dominik Żegleń <flesz3@o2.pl>
2017-04-08 20:24:07 +0200

    + ADDED AI SUPPORT

--- ai.py
+++ ai.py
@@ -1,1 +1,37 @@
-from sklearn.tree import DecisionTreeRegressor as DTR
+import numpy as np
+# from sklearn.tree import DecisionTreeRegressor as Model
+# from sklearn.linear_model import HuberRegressor as Model
+# from sklearn.linear_model import Ridge as Model
+from sklearn.svm import SVR as Model
+
+import matplotlib.pyplot as plt
+
+
+class Agent:
+    def __init__(self):
+        self.model = Model()
+
+    def fit(self, data):
+        train = []
+        values = []
+        weights = []
+        for bullet_id, bullet in data.items():
+            if bullet['hit']:
+                train.append(bullet['ast_angle'])
+                values.append(bullet['shoot_angle'])
+
+        train = np.asarray(train).reshape(-1, 1)
+        values = np.asarray(values).reshape(-1, 1)
+
+        # plt.scatter(train, values)
+        # plt.show()
+
+        print(np.hstack([train, values]))
+
+        self.model.fit(train, values)
+        print(self.model.score(train, values))
+
+        return self
+
+    def predict(self, angle):
+        return self.model.predict(angle)
--- sim/scene.py
+++ sim/scene.py
@@ -5,6 +5,8 @@ from pygame.locals import *
 from time import time
 from random import random
 from math import sin, cos, sqrt, atan2, exp
+from pickle import load, dump
+from ai import Agent
 
 res = (480, 480)
 fps = 30
@@ -12,6 +14,21 @@ colors = {
     'black': (0, 0, 0),
     'white': (255, 255, 255)
 }
+
+data = None
+try:
+    with open('sim/pickle_data/data.pck', 'rb+') as f:
+        try:
+            data = load(f)
+        except:
+            data = {}
+except:
+    data = {'0' : {
+        'shoot_angle': 0,
+        'ast_angle': 0,
+        'hit': 1
+    }}
+data_init_len = len(data)
 
 
 class Entity:
@@ -52,12 +69,16 @@ class Bullet(Entity):
     def check_alive(self):
         if sum([True if coord > r or coord < 0 else False for coord, r in zip(self.pos, res)]):
             self.sim.deregister(self.sim_id)
+            return False
+
         for entity in self.sim.entities:
             if not isinstance(entity, Controllable) and not isinstance(entity, Bullet):
-                if sqrt(sum((entity.get('pos') - self.pos + np.asarray(self.appearance.get_size()) / 2) ** 2)) <= entity.get('hit_radius'):
+                if sqrt(sum((entity.get('pos') - self.pos + np.asarray(
+                        self.appearance.get_size()) / 2) ** 2)) <= entity.get('hit_radius'):
                     self.sim.deregister(self.sim_id)
                     self.sim.deregister(entity.sim_id)
-                    break
+                    data[data_init_len + self.sim_id]['hit'] = 1
+                    return entity.sim_id
 
 
 class Controllable(Entity):
@@ -69,6 +90,22 @@ class Controllable(Entity):
                                         self.sim,
                                         init_pos=(self.pos + [x / 2 for x in self.original.get_size()]),
                                         init_speed=direction))
+
+
+class Watcher:
+    def __init__(self):
+        self.objects = []
+
+    def add(self, o):
+        self.objects.append(o)
+        return len(self.objects) - 1
+
+    def update(self, index, value):
+        self.objects[index] = value
+        return index
+
+    def get(self, index):
+        return self.objects[index]
 
 
 class Sim:
@@ -108,7 +145,21 @@ class Sim:
         return self.entities[id]
 
     def loop(self):
+        def spawn_asteroid():
+            asteroid = self.register(Entity('sim/img/zybel.png',
+                                            self.screen,
+                                            self,
+                                            init_pos=(_radial(np.array((240, 240)) - (120, 0) - (16, 16), random() * 2 * 3.141))))
+            self.entities[asteroid].set('on_deregister', spawn_asteroid)
+            watcher.update(1, watcher.get(1) + 1)
+            return watcher.update(0, self.entities[asteroid].sim_id)
+
+        watcher = Watcher()
+        watcher.objects.append(1)
+        watcher.objects.append(0)
+
         pg.font.init()
+        start_time = time()
         counter_time = time()
         running = True
 
@@ -116,29 +167,19 @@ class Sim:
                                           self.screen,
                                           self,
                                           init_pos=(np.array((240, 240)))))
-        asteroid = self.register(Entity('sim/img/zybel.png',
-                                        self.screen,
-                                        self,
-                                        init_pos=(np.array((240, 240)) - (120, 0))))
-        self.get_entity(asteroid).set('on_deregister', print_hit)
 
-        ast_speed = .3
-        bullets_per_second = 3
+        asteroid = spawn_asteroid()
 
-        data = {}
+        ast_speed = .4
+        bullets_per_second = 2
+        bullet_counter = 1
+        agent = Agent().fit(data)
 
         while running:
             try:
                 for event in pg.event.get():
                     if event.type == pg.QUIT:
                         running = False
-
-                    elif event.type == pg.MOUSEBUTTONDOWN:
-                        if pg.mouse.get_pressed()[0]:
-                            if time() - counter_time > 1. / bullets_per_second:
-                                counter_time = time()
-                                a = (_get_angle(pg.mouse.get_pos()))
-                                bullet_id = self.get_entity(char).fire(a, 7)
 
                     elif event.type == pg.KEYDOWN:
                         if event.key == pg.K_r:
@@ -149,32 +190,56 @@ class Sim:
             except SystemExit:
                 running = False
 
+            # AI
+            if time() - counter_time > 1. / bullets_per_second:
+                counter_time = time()
+                a = agent.predict(np.asarray([_get_angle(self.get_entity(watcher.get(asteroid)).get('pos'))]).reshape(-1, 1))
+                # a = random() * 3.141 * 2
+                bullet_id = self.get_entity(char).fire(a, 7)
+                data[data_init_len + self.entities[bullet_id].get('sim_id')] = {
+                    'shoot_angle': a,
+                    'ast_angle': _get_angle(self.get_entity(watcher.get(asteroid)).get('pos')),
+                    'hit': 0
+                }
+                bullet_counter += 1
+
             self.clock.tick(fps)
             self.screen.fill(colors['white'])
 
             # Draw helper lines
             pg.draw.line(self.screen, colors['black'], [0, 240], [480, 240])
             pg.draw.line(self.screen, colors['black'], [240, 0], [240, 480])
+            pg.draw.circle(self.screen, colors['black'], [240, 240], 136, 2)
 
             # Draw FPS counter
             pg.draw.rect(self.screen, colors['black'], [400, 0, 480, 30])
             font = pg.font.SysFont('arial', 15)
             self.screen.blit(font.render(str(self.clock.get_fps()), 1, colors['white']), (400, 0))
-            # ast_speed = ast_speed if random() > .025 else -ast_speed
-            self.get_entity(asteroid).set('pos',
-                                          _radial(self.get_entity(asteroid).get('pos'), 2 * 3.141 / fps * ast_speed))
+
+            # Draw kill counter
+            pg.draw.rect(self.screen, colors['black'], [0, 0, 80, 30])
+            font = pg.font.SysFont('arial', 15)
+            self.screen.blit(font.render(str(watcher.get(1) * 1. / bullet_counter), 1, colors['white']), (0, 0))
+
+            self.get_entity(watcher.get(asteroid)).set('pos',
+                                                       _radial(self.get_entity(watcher.get(asteroid)).get('pos'),
+                                                               2 * 3.141 / fps * ast_speed) )
 
             self.sim()
             pg.display.flip()
 
+        # with open('sim/pickle_data/data.pck', 'wb+') as f:
+        #     print('Dumping')
+        #     dump(data, f)
+
 
 def _radial(pos, angle, center=[x / 2 for x in res]):
     functions = [cos, sin]
-    relational = pos - np.asarray(center)
+    relational = pos - np.asarray(center) + (16, 16)
     r = sqrt(sum((relational) ** 2))
     curr_angle = atan2(relational[1], relational[0])
 
-    return np.asarray([f(curr_angle + angle) * r for f in functions]) + center
+    return np.asarray([f(curr_angle + angle) * r for f in functions]) + center - (16, 16)
 
 
 def _get_angle(pos, center=[x / 2 for x in res]):
